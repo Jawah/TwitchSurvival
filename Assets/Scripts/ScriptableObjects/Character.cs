a79491fcbbecc4c2221d3dd8e2654a37aefaab8e
unknown <nenad.slavujevic@posteo.de>
2018-11-19 15:28:41 +0100

    color overwork to black and white

---
+++ Assets/Scripts/ScriptableObjects/Character.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Character : ScriptableObject
+{
+    public string characterName;
+
+    public int dailyMoraleLossFactor;
+    public int dailyHealthLossFactor;
+}
