d33821143ec39b0de15c1d1c0e51a25db7dafa83
unknown <nenad.slavujevic@posteo.de>
2019-02-14 13:12:13 +0100

    started quotes implementation

---
+++ Assets/Scripts/Quotes.cs
@@ -0,0 +1,37 @@
+﻿public class Quotes
+{
+    public static string[] QUOTES_RANDOM = new string[]
+    {
+        "Crazy world!",
+        "What a day!",
+        "What's that?",
+        "What?",
+        "I see..."
+    };
+
+    public static string[] QUOTES_HUNGER_NEUTRAL = new string[]
+    {
+        "A snack would be nice",
+        "Do we have snacks?",
+        "My tummy is rumbling..."
+    };
+    public static string[] QUOTES_HUNGER_NEGATIV = new string[]
+    {
+        "I'm starving...",
+        "Food...",
+        "I yearn for a meal..."
+    };
+
+    public static string[] QUOTES_WARMTH_NEUTRAL = new string[]
+    {
+        "It's a bit fresh.",
+        "Can we close the window?",
+        "Turn the heat up a notch."
+    };
+    public static string[] QUOTES_WARMTH_NEGATIVE = new string[]
+    {
+        "It's freezing!",
+        "Let's snuggle!",
+        "We should get blankets."
+    };
+}
