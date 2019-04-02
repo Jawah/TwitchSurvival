414a216c67eafbd6d0c29524a83beb51e122f2d8
Nenad <nen.slavujevic@gmail.com>
2019-04-02 21:35:40 +0200

    started startmenulogic

---
+++ Assets/Scripts/MonoBehaviours/StartMenuLogic.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenuLogic : MonoBehaviour
+{
+    private int counter = 0;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            counter++;
+
+            switch (counter)
+            {
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+                    
+                    break;
+                case 4:
+
+                    break;
+            }
+        }
+    }
+}
